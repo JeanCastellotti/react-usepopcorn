fbb163b1d7ba484a27645e000794c659aefbe390
JeanDevFR <42603119+JeanDevFR@users.noreply.github.com>
2023-06-13 18:38:37 +0200

    remove useless style

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,7 @@
 function Loader() {
   return (
-    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-xl font-semibold uppercase">
-      <div className="h-10 w-10 animate-spin rounded-full border-4 border-white/20 border-t-white/80"></div>
+    <div className="absolute inset-0 flex">
+      <div className="m-auto h-10 w-10 animate-spin rounded-full border-4 border-white/20 border-t-white/80"></div>
     </div>
   )
 }
