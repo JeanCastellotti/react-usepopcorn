cdce5a031f454ac1aa958909dcc37af7dd56b05c
JeanDevFR <42603119+JeanDevFR@users.noreply.github.com>
2023-06-15 13:01:59 +0200

    add focus

--- src/components/HeaderSearch.jsx
+++ src/components/HeaderSearch.jsx
@@ -1,10 +1,34 @@
+import { useEffect } from 'react'
+import { useRef } from 'react'
+
 function HeaderSearch({ query, onSearch }) {
+  const input = useRef(null)
+
+  useEffect(() => {
+    const handler = (e) => {
+      if (document.activeElement === input.current) return
+
+      if (e.code === 'Enter') {
+        onSearch('')
+        input.current.focus()
+      }
+    }
+
+    document.addEventListener('keydown', handler)
+    input.current.focus()
+
+    return () => {
+      document.removeEventListener('keydown', handler)
+    }
+  }, [onSearch])
+
   return (
     <input
-      type="text"
-      placeholder="Search movies..."
+      ref={input}
       value={query}
       onChange={(e) => onSearch(e.target.value)}
+      type="text"
+      placeholder="Search movies..."
       className="w-[40rem] self-center rounded-[0.7rem] bg-[#7950f2] px-[1.6rem] py-[1.1rem] text-xl text-[#dee2e6] transition-all placeholder:text-[#adb5bd] focus:-translate-y-[2px]  focus:shadow-[0_2.4rem_2.4rem_rgba(0,0,0,0.1)] focus:outline-0"
     />
   )
